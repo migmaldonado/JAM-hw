2413ee74eec77c017f4310f5c55da25650e404d8
Miguel Maldonado <maldesan@gmail.com>
2018-10-13 21:14:27 -0400

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hello! I am Miguel</h1>
+    <h2>This is just a really quick intro</h2>
+    <p>I like Science, Data Science, and Web Development.</p>
+  </Layout>
+)
+
+export default IndexPage
